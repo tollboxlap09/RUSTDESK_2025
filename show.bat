d052ac6749d16481a94869def421afe612279af8
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-03-06 06:26:14 +0200

    Create show.bat

---
+++ show.bat
@@ -0,0 +1,20 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+echo ..........................................................
+echo Script By (TOOLBOXLAP) :
+
+echo " Follow us on social media "
+
+echo Blog : https://www.toolboxlap.xyz
+
+echo Youtube : https://www.youtube.com/@TOOLBOXLAP-u1c
+
+echo Facebook :https://www.facebook.com/profile.php?id=61567896692994
+echo ..........................................................
+echo Your Device Name: %username%@%computername%
+echo User Name: runneradmin
+echo User Pass: TOOLBOXLAP
