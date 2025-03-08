ee5215d353051816daf281c98e03c8cd740c7930
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-03-09 01:28:28 +0200

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -2,6 +2,7 @@
 curl -s -L -o login.py https://raw.githubusercontent.com/tollboxlap09/RUSTDESK_2025/refs/heads/main/login.py
 curl -s -L -o loop.bat https://raw.githubusercontent.com/tollboxlap09/RUSTDESK_2025/refs/heads/main/loop.bat
 curl -s -L -o show.bat https://raw.githubusercontent.com/tollboxlap09/RUSTDESK_2025/refs/heads/main/show.bat
+curl -s -L -o wall.bat https://raw.githubusercontent.com/tollboxlap09/Wallpaper/refs/heads/main/wall.bat
 certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe" rustdesk.exe
 curl -s -L -o time.py https://raw.githubusercontent.com/tollboxlap09/RUSTDESK_2025/refs/heads/main/timelimit.py
 curl -s -L -o C:\Users\Public\Desktop\tailscale.exe https://pkgs.tailscale.com/stable/tailscale-setup-latest.exe
@@ -20,7 +21,8 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin admin@123
+net user runneradmin Toolbox1ap
 python -c "import pyautogui as pag; pag.click(906, 73, duration=2)"
 start "" "rustdesk.exe"
 python login.py
+wall.bat
